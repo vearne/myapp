905f2373769c7be91a5e46d1d59544575bed9527
zhuwei01 <zhuwei01@qiyi.com>
2019-01-30 16:53:22 +0800

    update tool_server.py

--- tool_script/tool_server.py
+++ tool_script/tool_server.py
@@ -2,7 +2,8 @@
 #!/usr/bin/python
 from ip_tool  import int2ip, ip2int 
 from tornado.escape import json_encode, json_decode
-import tornado.ioloop
+#import tornado.ioloop
+from tornado import ioloop
 import tornado.web
 import sys
 import json
@@ -31,5 +32,9 @@ if __name__ == "__main__":
     port = 6060
     if len(sys.argv) > 1:
         port = int(sys.argv[1])
-    application.listen(port)
-    tornado.ioloop.IOLoop.instance().start()
+    #application.listen(port)
+    server = tornado.httpserver.HTTPServer(application)
+    server.bind(port)
+    #server.start(2)  # forks one process per cpu
+    server.start(2)  # forks one process per cpu
+    tornado.ioloop.IOLoop.current().start()
